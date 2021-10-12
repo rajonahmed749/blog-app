126da8fccfa83cbfcef276e7df9c5eee99ee8098
rajonahmed749 <raazon749@gmail.com>
2021-10-13 00:12:28 +0600

    creating front end with react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {
+  BrowserRouter as Router, Route
+} from "react-router-dom";
+//components 
+import Home from './pages/Home';
+import About from './pages/About';
+import Article from './pages/Article';
+import ArticlesList from './pages/ArticlesList';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="max-w-screen-md mx-auto pt-20" >
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/article-list" component={ArticlesList} />
+        <Route exact path="/article" component={Article} />
+
+      </div>
+    </Router>
   );
 }
 
