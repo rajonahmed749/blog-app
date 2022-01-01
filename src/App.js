8698f33229c32ea8aba96cbd5d98151716e4166f
rajonahmed749 <raazon749@gmail.com>
2022-01-01 23:34:44 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import {
-  BrowserRouter as Router, Route
-} from "react-router-dom";
-//Pages 
-import Home from './pages/Home';
-import About from './pages/About';
-import Article from './pages/Article';
-import ArticlesList from './pages/ArticlesList';
 
-// components
-import Navbar from "./components/Navbar"
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <div className="max-w-screen-md mx-auto pt-20" >
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/article-list" component={ArticlesList} />
-        <Route exact path="/article/:name" component={Article} />
-      </div>
-    </Router>
+    <div>
+      <h2>Hello , Piter</h2>
+    </div>
   );
 }
 
