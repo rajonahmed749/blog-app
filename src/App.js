f193cf6026eee8e54c47e708761e722a9177bd04
rajonahmed749 <raazon749@gmail.com>
2022-01-02 00:00:40 +0600

    initial structure done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import Home from "./components/Home/Home";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import TechNews from "./components/News/TechNews";
+import Menubar from "./components/Common/Menubar/Menubar";
 
 function App() {
   return (
     <div>
-      <h2>Hello , Piter</h2>
+      <Router>
+        <Menubar />
+        <Switch>
+          <Route path="/news">
+            <TechNews />
+          </Route>
+          <Route path="/users">
+            {/* <Users /> */}
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/News/TechNews.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TechNews = () => {
+    return (
+        <div>
+            thiis is tech news
+        </div>
+    )
+}
+
+export default TechNews
